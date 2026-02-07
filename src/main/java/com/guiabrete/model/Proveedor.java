4156107879d1ca2a9281cfe02e7a7f9716bff2ea
Angely Perez <guidoangely1987@gmail.com>
2026-02-06 22:32:34 -0600

    Clase proveedor y  visitante: Listo

--- src/main/java/com/guiabrete/model/Proveedor.java
+++ src/main/java/com/guiabrete/model/Proveedor.java
@@ -1,4 +1,31 @@
 package com.guiabrete.model;
 
-public class Proveedor {
+public class Proveedor extends Usuario{
+    private String zona;
+    private String horario;
+
+    public Proveedor() {
+    }
+
+    public Proveedor(int idUsuario, String nombre, String telefono, String email, String contrasenia,  String zona, String horario) {
+        super(idUsuario, nombre, telefono, email, contrasenia);
+        this.zona = zona;
+        this.horario = horario;
+    }
+
+    public String getZona() {
+        return zona;
+    }
+
+    public void setZona(String zona) {
+        this.zona = zona;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
 }
--- src/main/java/com/guiabrete/model/Visitante.java
+++ src/main/java/com/guiabrete/model/Visitante.java
@@ -1,4 +1,11 @@
 package com.guiabrete.model;
 
-public class Visitante {
+public class Visitante extends Usuario{
+
+    public Visitante() {
+    }
+
+    public Visitante(int idUsuario, String nombre, String telefono, String email, String contrasenia) {
+        super(idUsuario, nombre, telefono, email, contrasenia);
+    }
 }
