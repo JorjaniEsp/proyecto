d1ee13926fc76fc632cfaac9c0eb2b1fb4d9262b
Angely Perez <guidoangely1987@gmail.com>
2026-02-06 22:19:36 -0600

    Clase Usuario y Enum: listo

--- src/main/java/com/guiabrete/model/Usuario.java
+++ src/main/java/com/guiabrete/model/Usuario.java
@@ -1,4 +1,60 @@
 package com.guiabrete.model;
 
 public class Usuario {
+
+    private int idUsuario;
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String contrasenia;
+
+    public Usuario() {
+    }
+    public Usuario(int idUsuario, String nombre, String telefono, String email, String contrasenia) {
+        this.idUsuario = idUsuario;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.email = email;
+        this.contrasenia = contrasenia;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
 }
