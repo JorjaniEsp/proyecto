ff8edb512e1b5e3201bde26054d2b3ef17fb1acb
Angely Perez <guidoangely1987@gmail.com>
2026-02-09 19:48:34 -0600

    Clase ControladorApp: Firma de metodos, listos

--- src/main/java/com/guiabrete/controller/ControladorApp.java
+++ src/main/java/com/guiabrete/controller/ControladorApp.java
@@ -1,4 +1,76 @@
 package com.guiabrete.controller;
 
+import com.guiabrete.MainApp;
+import com.guiabrete.model.*;
+
+import java.util.List;
+
 public class ControladorApp {
+
+    private RepositorioUsuarios repoUsuarios;
+    private RepositorioServicios repoServicios;
+    private PersistenciaArchivo persistencia;
+    private MainApp vistaPrincipal;
+
+    public ControladorApp() {
+    }
+
+    public ControladorApp(RepositorioUsuarios repoUsuarios, RepositorioServicios repoServicios, PersistenciaArchivo persistencia, MainApp vistaPrincipal) {
+        this.repoUsuarios = repoUsuarios;
+        this.repoServicios = repoServicios;
+        this.persistencia = persistencia;
+        this.vistaPrincipal = vistaPrincipal;
+    }
+
+    public void iniciar(){
+
+    }
+
+    public void registrarUsuario(){
+
+    }
+
+    public void iniciarSesionProveedor(){
+
+    }
+
+    public void anadirServicio(){
+
+    }
+
+    public void modificarServicio(){
+
+    }
+
+    public void eliminarServicio(){
+
+    }
+
+    public void mostrarCatalogo(){
+
+    }
+
+    public List<Servicio> buscarPorZona(String zona){
+        return null;
+    }
+
+    public List<Servicio> buscarPorCategoria(Categoria cat){
+        return null;
+    }
+
+    public void verDetalleServidor(Servicio serv){
+
+    }
+
+    public void contactarProveedor(Servicio serv){
+
+    }
+
+    public void actualizarPerfilProveedor(Proveedor p){
+
+    }
+
+    public List<Servicio> verServiciosPropios(Proveedor p){
+        return null;
+    }
 }
