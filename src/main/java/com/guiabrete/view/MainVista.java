84809b2d3c1350789e1a99677332be5c15cda1e1
Angely Perez <guidoangely1987@gmail.com>
2026-02-09 20:01:19 -0600

    Clase MainVista: Firma de metodos, listos

--- src/main/java/com/guiabrete/view/MainVista.java
+++ src/main/java/com/guiabrete/view/MainVista.java
@@ -1,4 +1,56 @@
 package com.guiabrete.view;
 
+import com.guiabrete.model.Proveedor;
+import com.guiabrete.model.Servicio;
+
+import java.util.List;
+
 public class MainVista {
+
+    public MainVista() {
+    }
+
+    public void mostrarMenuPrincipal(){
+
+    }
+
+    public void mostrarPantallaInicioSesion(){
+
+    }
+    public void mostrarFormularioRegistroVisitante(){
+
+    }
+
+    public void mostrarFormularioRegistroProveedor(){
+
+    }
+
+    public void mostrarPantallaLoginProveedor(){
+
+    }
+
+    public void mostrarPanelVisitante(List<Servicio> lista){
+
+    }
+
+    public void mostrarPanelProveedor(List<Servicio> lista){
+
+    }
+
+    public void mostrarFormularioServicio(Servicio serv){
+
+    }
+
+    public void mostrarDetalleServicio(Servicio serv){
+
+    }
+
+    public void mostrarPerfilProveedor(Proveedor p){
+
+    }
+
+    public void mostrarMensaje(String mensaje){
+
+    }
+
 }
