b3bd1e7f6581aab6f554ca364de20e71a19927d6
JorjaniEsp <espinozajorjanie2006@gmail.com>
2026-02-09 20:04:37 -0600

    correcion

--- src/main/java/com/guiabrete/view/MainVista.java
+++ src/main/java/com/guiabrete/view/MainVista.java
@@ -7,50 +7,6 @@ import java.util.List;
 
 public class MainVista {
 
-    public MainVista() {
-    }
 
-    public void mostrarMenuPrincipal(){
-
-    }
-
-    public void mostrarPantallaInicioSesion(){
-
-    }
-    public void mostrarFormularioRegistroVisitante(){
-
-    }
-
-    public void mostrarFormularioRegistroProveedor(){
-
-    }
-
-    public void mostrarPantallaLoginProveedor(){
-
-    }
-
-    public void mostrarPanelVisitante(List<Servicio> lista){
-
-    }
-
-    public void mostrarPanelProveedor(List<Servicio> lista){
-
-    }
-
-    public void mostrarFormularioServicio(Servicio serv){
-
-    }
-
-    public void mostrarDetalleServicio(Servicio serv){
-
-    }
-
-    public void mostrarPerfilProveedor(Proveedor p){
-
-    }
-
-    public void mostrarMensaje(String mensaje){
-
-    }
 
 }
