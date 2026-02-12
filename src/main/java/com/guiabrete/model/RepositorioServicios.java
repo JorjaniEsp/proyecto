d14a8f520efbf30cfd95d75575d245e16bbcb373
JorjaniEsp <espinozajorjanie2006@gmail.com>
2026-02-12 15:00:41 -0600

    repositorio servicios lista, pendiente a cambios

--- src/main/java/com/guiabrete/model/RepositorioServicios.java
+++ src/main/java/com/guiabrete/model/RepositorioServicios.java
@@ -20,41 +20,68 @@ public class RepositorioServicios {
     }
 
     public void agregarServ(Servicio servicio){
-
+        if (servicio != null){
+            servicios.add(servicio);
+        }
     }
 
     public List<Servicio> buscarPorZona(String zona){
-        return null;
+        if (zona.isBlank()){
+            return servicios;
+        } else {
+          return servicios.stream().filter(serv -> serv.getZona().equalsIgnoreCase(zona)).toList();
+        }
     }
 
     public List<Servicio> buscarPorCategoria(Categoria categoria){
-        return null;
+            return servicios.stream().filter(serv -> serv.getCategoria().equals(categoria)).toList();
     }
 
     public List<Servicio> buscarPorTexto(String texto){
-        return null;
+        if (texto.isBlank()){
+            return servicios;
+        } else {
+            return servicios.stream().filter(serv -> serv.getDescripcionServ().toLowerCase().contains(texto.toLowerCase())).toList();
+        }
     }
 
     public void editarServicio(Servicio servicio){
-
+        etiqueta:
+        for (Servicio serv : servicios){
+            if (serv.equals(servicio)){
+                serv.setNombreServ(servicio.getNombreServ());
+                serv.setDescripcionServ(servicio.getDescripcionServ());
+                serv.setCategoria(servicio.getCategoria());
+                serv.setContacto(servicio.getContacto());
+                serv.setHorario(servicio.getHorario());
+                serv.setZona(servicio.getZona());
+                break etiqueta;
+            }
+        }
     }
 
-    public void eliminarServicio(Servicio servicio){
-
+    public void eliminarServicio(Servicio servicio) {
+        if (servicio != null) {
+            servicios.remove(servicio);
+        }
     }
 
     public List<Servicio> obtenerTodos(){
-        return null;
+        return servicios;
     }
 
     public int generarIdServicio(){
-        return 0;
+        return siguienteIdServ++;
     }
 
     public void inicializarIdServicio(){
+        int maxId = 0;
 
+        for (Servicio s : servicios) {
+            if (s.getIdServ() > maxId) {
+                maxId = s.getIdServ();
+            }
+        }
+        this.siguienteIdServ = maxId + 1;
     }
-
-
-
 }
