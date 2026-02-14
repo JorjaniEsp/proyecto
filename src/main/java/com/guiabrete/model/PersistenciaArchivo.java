c9baf783802b6ca3010d72373f1fa77e0774becb
Angely Perez <guidoangely1987@gmail.com>
2026-02-13 22:11:09 -0600

    PersistenciaArchivos: metodos cargarUsuarios y guardarUsuarios, listos. Clase lista

--- src/main/java/com/guiabrete/model/PersistenciaArchivo.java
+++ src/main/java/com/guiabrete/model/PersistenciaArchivo.java
@@ -27,12 +27,52 @@ public class PersistenciaArchivo {
         return instance;
     }
 
-    public void cargarUsurios(RepositorioUsuarios repoUsuarios){
-
+    public void cargarUsurios(RepositorioUsuarios repoUsuarios) throws IOException {
+        if (usuarios.exists()) {
+            try (BufferedReader lector = new BufferedReader(new FileReader(usuarios))) {
+                String linea;
+                while ((linea = lector.readLine()) != null) {
+                    String[] infoUsuario = linea.split("\\|");
+                    if (infoUsuario[0].equals("PROVEEDOR")) {
+                        int id = Integer.parseInt(infoUsuario[1]);
+                        String nombre = infoUsuario[2];
+                        String telefono = infoUsuario[3];
+                        String email = infoUsuario[4];
+                        String contrasena = infoUsuario[5];
+                        String zona = infoUsuario[6];
+                        String horario = infoUsuario[7];
+                        repoUsuarios.anadirProveedor(new Proveedor(id, nombre, telefono, email, contrasena, zona, horario));
+                    } else if (infoUsuario[0].equals("VISITANTE")) {
+                        int id = Integer.parseInt(infoUsuario[1]);
+                        String nombre = infoUsuario[2];
+                        String telefono = infoUsuario[3];
+                        String email = infoUsuario[4];
+                        String contrasena = infoUsuario[5];
+                        repoUsuarios.anadirVisitante(new Visitante(id, nombre, telefono, email, contrasena));
+                    }
+                }
+            } catch (IOException e) {
+                throw new IOException("Error al cargar usuarios: " + e.getMessage());
+            }
+        }
     }
 
-    public void guardarUsuarios(RepositorioUsuarios repoUsuarios){
+    public void guardarUsuarios(RepositorioUsuarios repoUsuarios) throws IOException {
+        List<Proveedor> listaProveedorGuardar = repoUsuarios.obtenerProveedor();
+        List<Visitante> listaVisitanteGuardar = repoUsuarios.obtenerVisitante();
 
+        try (BufferedWriter escritor = new BufferedWriter(new FileWriter(usuarios, false))) {
+            for (Proveedor proveedor : listaProveedorGuardar) {
+                escritor.write(proveedor.toString());
+                escritor.newLine();
+            }
+            for (Visitante visitante : listaVisitanteGuardar) {
+                escritor.write(visitante.toString());
+                escritor.newLine();
+            }
+        } catch (IOException e) {
+            throw new IOException("Error al escribir en el archivo de usuarios: " + e.getMessage());
+        }
     }
 
     public void cargarServicios(RepositorioServicios repoServicios, RepositorioUsuarios repoUsuarios) throws IOException {
--- src/main/java/com/guiabrete/model/Proveedor.java
+++ src/main/java/com/guiabrete/model/Proveedor.java
@@ -31,6 +31,6 @@ public class Proveedor extends Usuario{
 
     @Override
     public String toString() {
-        return getIdUsuario()+ "\\|" + getNombre() + "\\|" + getTelefono() + "\\|" + getEmail() + "\\|" + getContrasenia() + "\\|" + zona + "\\|" + horario;
+        return "PROVEEDOR" + "\\|" + getIdUsuario()+ "\\|" + getNombre() + "\\|" + getTelefono() + "\\|" + getEmail() + "\\|" + getContrasenia() + "\\|" + zona + "\\|" + horario;
     }
 }
