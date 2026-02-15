7397e20c33aa04315d5983bcc006e41ff545f016
JorjaniEsp <espinozajorjanie2006@gmail.com>
2026-02-15 16:38:11 -0600

    Correcion del super del constructor del vistante

--- src/main/java/com/guiabrete/model/Visitante.java
+++ src/main/java/com/guiabrete/model/Visitante.java
@@ -5,7 +5,7 @@ public class Visitante extends Usuario{
     public Visitante() {
     }
 
-    public Visitante(int idUsuario, String nombre, String telefono, String email, String contrasenia) {
+    public Visitante(int idUsuario, String nombre, String telefono, String email, String contrasenia) throws GuiaBreteException {
         super(idUsuario, nombre, telefono, email, contrasenia);
     }
 
